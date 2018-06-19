a11d06f86437c87fe738f8daa441b10c5ae8baa2
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-19 15:42:21 +0100

    Delete unnecessary folders, Test quality depreciation of standard item

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,18 @@
 describe("Gilded Rose", function() {
 
+  describe("updateQuality", function() {
+    it("Should decrease the quality of a standard item by 1", function() {
+      const gildedRose = new Shop([ new Item("sword", 5, 5)]);
+      gildedRose.updateQuality()
+      expect(gildedRose.items[0].quality).toEqual(4)
+    })
+
+  })
+
   it("should foo", function() {
     const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].name).toEqual("foo");
   });
 
 });
