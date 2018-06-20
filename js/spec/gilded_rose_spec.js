27a6d4026265c99c7f5f33bb933999ddb0fb8ed1
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-20 14:47:51 +0100

    Reorganise tests

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -2,118 +2,151 @@ describe("Gilded Rose", function() {
 
   describe("updateQuality", function() {
 
-    describe("Within SellIn value", function() {
+    describe("Standard Items", function() {
 
-      it("Should decrease the quality of a standard item by 1", function() {
-        const gildedRose = new Shop([ new Item("sword", 5, 5)]);
-        gildedRose.updateQuality()
-        expect(gildedRose.items[0].quality).toEqual(4)
-      });
-
-      it("Should increase the quality of Aged Brie by 1", function() {
-        const gildedRose = new Shop([ new Item("Aged Brie", 5, 5)]);
-        gildedRose.updateQuality()
-        expect(gildedRose.items[0].quality).toEqual(6)
-      });
-
-      it("Should no longer see its value depreciate after reaching 0", function() {
-        const gildedRose = new Shop([ new Item("sword", 5, 1)]);
-        gildedRose.updateQuality()
-        gildedRose.updateQuality()
-        expect(gildedRose.items[0].quality).toEqual(0)
-      });
-
-      it("Should cap quality at 50", function() {
-        const gildedRose = new Shop([ new Item("Aged Brie", 5, 50)]);
-        gildedRose.updateQuality()
-        expect(gildedRose.items[0].quality).toEqual(50)
-      })
-
-      it("Should depreciate in value twice as fast", function() {
-        const gildedRose = new Shop([ new Item("sword", 0, 5)]);
-        gildedRose.updateQuality()
-        gildedRose.updateQuality()
-        expect(gildedRose.items[0].quality).toEqual(1)
-      });
-
-      it("Should not depreciate in quality", function() {
-        const gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 5, 5)]);
-        gildedRose.updateQuality()
-        expect(gildedRose.items[0].quality).toEqual(5)
-      });
-
-        describe("SellIn value is greater than 10", function() {
-          it("Should increase the quality of Backstage passes by 1", function() {
-            const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 20, 5)]);
-            gildedRose.updateQuality()
-            expect(gildedRose.items[0].quality).toEqual(6)
-          });
+      describe("Within SellIn value", function() {
+        it("Should decrease the quality of a standard item by 1", function() {
+          const gildedRose = new Shop([ new Item("sword", 5, 5)]);
+          gildedRose.updateQuality()
+          expect(gildedRose.items[0].quality).toEqual(4)
         });
 
-        describe("SellIn value is less than 10 and greater than 5", function() {
-          it("Should increase the quality of Backstage passes by 2", function() {
-            const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 9, 5)]);
-            gildedRose.updateQuality()
-            expect(gildedRose.items[0].quality).toEqual(7)
-          });
+        it("Should no longer see its value depreciate after reaching 0", function() {
+          const gildedRose = new Shop([ new Item("sword", 5, 1)]);
+          gildedRose.updateQuality()
+          gildedRose.updateQuality()
+          expect(gildedRose.items[0].quality).toEqual(0)
+        });
+      });
+
+      describe("After SellIn value = 0", function() {
+        it("Should depreciate in value twice as fast", function() {
+          const gildedRose = new Shop([ new Item("sword", 0, 5)]);
+          gildedRose.updateQuality()
+          gildedRose.updateQuality()
+          expect(gildedRose.items[0].quality).toEqual(1)
+        });
+      });
+
+      describe("SellIn value", function() {
+        it('Decreases by 1 after an update', function() {
+          const gildedRose = new Shop([ new Item("sword", 10, 10)]);
+          gildedRose.updateQuality()
+          expect(gildedRose.items[0].sellIn).toEqual(9)
+        });
+      });
+    });
+
+    describe("Aged Brie", function() {
+
+      describe("Within SellIn value", function() {
+        it("Should increase the quality of Aged Brie by 1", function() {
+          const gildedRose = new Shop([ new Item("Aged Brie", 5, 5)]);
+          gildedRose.updateQuality()
+          expect(gildedRose.items[0].quality).toEqual(6)
         });
 
-        describe("SellIn value is less than 5 and greater than 0", function() {
-          it("Should increase the quality of Backstage passes by 3", function() {
-            const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 4, 5)]);
-            gildedRose.updateQuality()
-            expect(gildedRose.items[0].quality).toEqual(8)
-          });
-
-          it("Should keep quality of Backstage passes 50", function() {
-            const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 4, 49)]);
-            gildedRose.updateQuality()
-            expect(gildedRose.items[0].quality).toEqual(50)
-          });
+        it("Should cap quality at 50", function() {
+          const gildedRose = new Shop([ new Item("Aged Brie", 5, 50)]);
+          gildedRose.updateQuality()
+          expect(gildedRose.items[0].quality).toEqual(50)
         });
-    });
-
-
-    describe("SellIn value is 0 or less", function() {
-      it("Should decrease the quality of Backstage passes to 0", function() {
-        const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)]);
-        gildedRose.updateQuality()
-        expect(gildedRose.items[0].quality).toEqual(0)
       });
 
-      it("Should decrease the quality of standard items by 2", function() {
-        const gildedRose = new Shop([ new Item("sword", 0, 10)]);
-        gildedRose.updateQuality()
-        expect(gildedRose.items[0].quality).toEqual(8)
-      });
-
-      it("Should not change the quality of Sulfuras", function() {
-        const gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 0, 10)]);
-        gildedRose.updateQuality()
-        expect(gildedRose.items[0].quality).toEqual(10)
+      describe("After SellIn value = 0", function() {
+        it("Should continue to increase in quality by 1", function() {
+          const gildedRose = new Shop([ new Item("Aged Brie", 0, 10)]);
+          gildedRose.updateQuality()
+          expect(gildedRose.items[0].quality).toEqual(11)
+        });
       });
     });
-  });
 
-  describe('SellIn', function() {
-    it('Decreases by 1 after an update', function() {
-      const gildedRose = new Shop([ new Item("sword", 10, 10)]);
-      gildedRose.updateQuality()
-      expect(gildedRose.items[0].sellIn).toEqual(9)
+    describe("Backstage Passes", function() {
+
+      describe("SellIn value is greater than 10", function() {
+        it("Should increase the quality of Backstage passes by 1", function() {
+          const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 20, 5)]);
+          gildedRose.updateQuality()
+          expect(gildedRose.items[0].quality).toEqual(6)
+        });
+      });
+
+      describe("SellIn value is less than 10 and greater than 5", function() {
+        it("Should increase the quality of Backstage passes by 2", function() {
+          const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 9, 5)]);
+          gildedRose.updateQuality()
+          expect(gildedRose.items[0].quality).toEqual(7)
+        });
+      });
+
+      describe("SellIn value is less than 5 and greater than 0", function() {
+        it("Should increase the quality of Backstage passes by 3", function() {
+          const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 4, 5)]);
+          gildedRose.updateQuality()
+          expect(gildedRose.items[0].quality).toEqual(8)
+        });
+
+        it("Should keep quality of Backstage passes at 50", function() {
+          const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 4, 49)]);
+          gildedRose.updateQuality()
+          expect(gildedRose.items[0].quality).toEqual(50)
+        });
+      });
+
+      describe("SellIn value is 0 or less", function() {
+        it("Should decrease the quality of Backstage passes to 0", function() {
+          const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)]);
+          gildedRose.updateQuality()
+          expect(gildedRose.items[0].quality).toEqual(0)
+        });
+      });
     });
 
-    it('Holds its value after an update', function() {
-      const gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 10, 10)]);
-      gildedRose.updateQuality()
-      expect(gildedRose.items[0].sellIn).toEqual(10)
+    describe("Sulfuras", function() {
+
+      describe("Within SellIn value", function() {
+        it("Should not depreciate in quality", function() {
+          const gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 5, 5)]);
+          gildedRose.updateQuality()
+          expect(gildedRose.items[0].quality).toEqual(5)
+        });
+      });
+
+      describe("SellIn value is 0 or less", function() {
+        it("Should not depreciate in quality", function() {
+          const gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 0, 10)]);
+          gildedRose.updateQuality()
+          expect(gildedRose.items[0].quality).toEqual(10)
+        });
+      });
+
+      describe('SellIn', function() {
+        it('Holds its value after an update', function() {
+          const gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 10, 10)]);
+          gildedRose.updateQuality()
+          expect(gildedRose.items[0].sellIn).toEqual(10)
+        });
+      });
     });
   });
 
   describe('Conjured items', function() {
-    it('Should depreciate in value twice as fast', function() {
-      const gildedRose = new Shop([ new Item("Conjured sword", 10, 10)]);
-      gildedRose.updateQuality()
-      expect(gildedRose.items[0].quality).toEqual(8)
+
+    describe('Within SellIn date', function() {
+      it('Should depreciate in value twice as fast', function() {
+        const gildedRose = new Shop([ new Item("Conjured sword", 10, 10)]);
+        gildedRose.updateQuality()
+        expect(gildedRose.items[0].quality).toEqual(8)
+      });
+    });
+
+    describe('SellIn date <= 0', function() {
+      it('Should depreciate in value twice as fast', function() {
+        const gildedRose = new Shop([ new Item("Conjured sword", 0, 10)]);
+        gildedRose.updateQuality()
+        expect(gildedRose.items[0].quality).toEqual(6)
+      });
     });
 
     it('Should remain unchanged', function() {
