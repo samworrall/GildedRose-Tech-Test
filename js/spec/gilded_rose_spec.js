ae83ba2e8d2f594fc266ffa0b929d6524ae1c708
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-21 10:53:01 +0100

    Update tests for StandardItems

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -113,6 +113,24 @@ describe("StandardItem", function() {
     expect(standarditem.quality).toEqual(9)
   });
 
+  it("Decreases the quality by 2", function() {
+    const standarditem = new StandardItem('sword', 0, 10)
+    standarditem.update();
+    expect(standarditem.quality).toEqual(8)
+  });
+
+  it("Decreases the quality by 2 for conjured items", function() {
+    const standarditem = new StandardItem('Conjured sword', 5, 10)
+    standarditem.update();
+    expect(standarditem.quality).toEqual(8)
+  });
+
+  it("Decreases the quality by 4 for conjured items", function() {
+    const standarditem = new StandardItem('Conjured sword', 0, 10)
+    standarditem.update();
+    expect(standarditem.quality).toEqual(6)
+  });
+
   it("decreases the sellIn by 1", function() {
     const standarditem = new StandardItem('sword', 5, 10)
     standarditem.update();
