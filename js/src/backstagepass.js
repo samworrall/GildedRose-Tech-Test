f33bcc144edfb34593fc77efe611ce271714ea50
Edward Andress <eddie_andress@hotmail.com>
2018-06-21 17:31:35 +0100

    Proposal from Mark, via Eddie.
    Obviously this is just a sketch of what kind of thing might be possible.  This is a strategy pattern.

--- js/src/backstagepass.js
+++ js/src/backstagepass.js
@@ -1,32 +1,24 @@
 class BackstagePass extends Item {
-  constructor(name, sellIn, quality) {
+  constructor(name, sellIn, quality, rules) {
     super(name, sellIn, quality);
+    this.rules = rules;
   }
 
   update() {
-    switch(this.sellIn <= 0) {
-      case true:
-        this.quality = 0
-        break;
-      case false:
-        switch(this.quality < 50) {
-          case true:
-            increase_quality();
-            switch(this.sellIn <= 10 && this.quality < 50) {
-              case true:
-                increase_quality();
-                switch(this.sellIn <= 5 && this.quality < 50) {
-                  case true:
-                    increase_quality();
-                    break;
-                }
-            }
-        }
-    }
-    this.sellIn -= 1
+    this.quality = this.rules
+      .filter((rule) => rule.applies(sellIn, quality))
+      .reduce((rule) => rule.apply(quality), this.quality)
+  }
+}
+
+// rule.js
+class Rule {
+
+  applies(sellIn, quality) {
+    return sellIn < 5 && quality > 20;
   }
 
-  increase_quality() {
-    this.quality += 1
+  apply(quality) {
+    return quality + 1;
   }
 }
