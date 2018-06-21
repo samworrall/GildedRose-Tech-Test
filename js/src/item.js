0e1172c8c9765cf46e7b40567068bde440cbba36
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-21 11:21:47 +0100

    Split Item class into its own file

---
+++ js/src/item.js
@@ -0,0 +1,78 @@
+// (function(exports) {
+
+class Item {
+  constructor(name, sellIn, quality){
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
+}
+
+class AgedBrie extends Item {
+  constructor(name, sellIn, quality) {
+    super(name, sellIn, quality);
+  }
+
+  update() {
+    if (this.quality < 50) {
+      this.quality += 1
+    }
+    this.sellIn -= 1
+  }
+}
+
+class BackstagePass extends Item {
+  constructor(name, sellIn, quality) {
+    super(name, sellIn, quality);
+  }
+
+  update() {
+    if (this.sellIn <= 0) {
+      this.quality = 0
+    } else if (this.quality < 50) {
+      this.quality += 1
+      if (this.sellIn <= 10 && this.quality < 50) {
+        this.quality += 1
+        if (this.sellIn <= 5 && this.quality < 50) {
+          this.quality += 1
+        }
+      }
+    }
+    this.sellIn -= 1
+  }
+}
+
+class StandardItem extends Item {
+  constructor(name, sellIn, quality) {
+    super(name, sellIn, quality);
+  }
+
+  update() {
+    var multiplier = 1
+    if (this.name.includes('Conjured')) {
+      multiplier = 2
+    }
+    if (this.sellIn <= 0 && this.quality > 1) {
+      this.quality -= 2 * multiplier
+    } else if (this.quality > 0) {
+      this.quality -= 1 * multiplier
+    }
+    this.sellIn -= 1
+  }
+}
+
+class LegendaryItem extends Item {
+  constructor(name, sellIn, quality) {
+    super(name, sellIn, quality);
+  }
+
+  update() {
+  }
+}
+
+// exports.Item = Item;
+// exports.AgedBrie = AgedBrie;
+// exports.BackstagePass = BackstagePass;
+// exports.StandardItem = StandardItem;
+// exports.LegendaryItem = LegendaryItem;
+// })(this);
