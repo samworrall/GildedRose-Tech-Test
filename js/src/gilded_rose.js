a52030408529e2a7cf9660a6b3e83acb21bd98f7
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-21 10:50:58 +0100

    Add update method to BackstagePass class

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -23,6 +23,21 @@ class BackstagePass extends Item {
   constructor(name, sellIn, quality) {
     super(name, sellIn, quality);
   }
+
+  update() {
+    if (this.sellIn <= 0) {
+      this.quality = 0
+    } else if (this.quality < 50) {
+      this.quality += 1
+      if (this.sellIn <= 10 && this.quality < 50) {
+        this.quality += 1
+        if (this.sellIn <= 5 && this.quality < 50) {
+          this.quality += 1
+        }
+      }
+    }
+    this.sellIn -= 1
+  }
 }
 
 class StandardItem extends Item {
