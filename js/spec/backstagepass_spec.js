ba05942f65e30e8e84fb3b524cf6e155d803e038
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-22 11:57:25 +0100

    Update StandardItem tests

--- js/spec/backstagepass_spec.js
+++ js/spec/backstagepass_spec.js
@@ -14,34 +14,36 @@ describe("BackstagePass", function() {
     expect(backstagepass.quality).toEqual(10)
   });
 
-  it("Increases the quality by 1", function() {
-    const backstagepass = new BackstagePass('BackstagePass', 11, 10)
-    backstagepass.update();
-    expect(backstagepass.quality).toEqual(11)
-  });
+  describe("#update", function() {
+    it("Increases the quality by 1", function() {
+      const backstagepass = new BackstagePass('BackstagePass', 11, 10)
+      backstagepass.update();
+      expect(backstagepass.quality).toEqual(11)
+    });
 
-  it("Increases the quality by 2", function() {
-    const backstagepass = new BackstagePass('BackstagePass', 10, 10)
-    backstagepass.update();
-    expect(backstagepass.quality).toEqual(12)
-  });
+    it("Increases the quality by 2", function() {
+      const backstagepass = new BackstagePass('BackstagePass', 10, 10)
+      backstagepass.update();
+      expect(backstagepass.quality).toEqual(12)
+    });
 
-  it("Increases the quality by 3", function() {
-    const backstagepass = new BackstagePass('BackstagePass', 5, 10)
-    backstagepass.update();
-    expect(backstagepass.quality).toEqual(13)
-  });
+    it("Increases the quality by 3", function() {
+      const backstagepass = new BackstagePass('BackstagePass', 5, 10)
+      backstagepass.update();
+      expect(backstagepass.quality).toEqual(13)
+    });
 
-  it("Decreases the quality to 0", function() {
-    const backstagepass = new BackstagePass('BackstagePass', 0, 10)
-    backstagepass.update();
-    expect(backstagepass.quality).toEqual(0)
-  });
+    it("Decreases the quality to 0", function() {
+      const backstagepass = new BackstagePass('BackstagePass', 0, 10)
+      backstagepass.update();
+      expect(backstagepass.quality).toEqual(0)
+    });
 
-  it("Decreses the sellIn by 1", function() {
-    const backstagepass = new BackstagePass('BackstagePass', 5, 10)
-    backstagepass.update();
-    expect(backstagepass.sellIn).toEqual(4)
+    it("Decreses the sellIn by 1", function() {
+      const backstagepass = new BackstagePass('BackstagePass', 5, 10)
+      backstagepass.update();
+      expect(backstagepass.sellIn).toEqual(4)
+    });
   });
 
   describe("#increase_quality", function() {
--- js/spec/standarditem_spec.js
+++ js/spec/standarditem_spec.js
@@ -14,33 +14,97 @@ describe("StandardItem", function() {
     expect(standarditem.quality).toEqual(10)
   });
 
-  it("Decreases the quality by 1", function() {
-    const standarditem = new StandardItem('sword', 5, 10)
-    standarditem.update();
-    expect(standarditem.quality).toEqual(9)
+  describe("#update", function() {
+    it("Decreases the quality by 1", function() {
+      const standarditem = new StandardItem('sword', 5, 10)
+      standarditem.update();
+      expect(standarditem.quality).toEqual(9)
+    });
+
+    it("Decreases the quality by 2", function() {
+      const standarditem = new StandardItem('sword', 0, 10)
+      standarditem.update();
+      expect(standarditem.quality).toEqual(8)
+    });
+
+    it("Decreases the quality by 2 for conjured items", function() {
+      const standarditem = new StandardItem('Conjured sword', 5, 10)
+      standarditem.update();
+      expect(standarditem.quality).toEqual(8)
+    });
+
+    it("Decreases the quality by 4 for conjured items", function() {
+      const standarditem = new StandardItem('Conjured sword', 0, 10)
+      standarditem.update();
+      expect(standarditem.quality).toEqual(6)
+    });
+
+    it("Decreases the sellIn by 1", function() {
+      const standarditem = new StandardItem('sword', 5, 10)
+      standarditem.update();
+      expect(standarditem.sellIn).toEqual(4)
+    });
   });
 
-  it("Decreases the quality by 2", function() {
-    const standarditem = new StandardItem('sword', 0, 10)
-    standarditem.update();
-    expect(standarditem.quality).toEqual(8)
+  describe("#is_a_conjured_item", function() {
+    it("Returns true", function() {
+      const standarditem = new StandardItem('Conjured sword', 5, 10)
+      expect(standarditem.is_a_conjured_item()).toEqual(true)
+    });
+
+    it("Returns false", function() {
+      const standarditem = new StandardItem('sword', 5, 10)
+      expect(standarditem.is_a_conjured_item()).toEqual(false)
+    });
   });
 
-  it("Decreases the quality by 2 for conjured items", function() {
-    const standarditem = new StandardItem('Conjured sword', 5, 10)
-    standarditem.update();
-    expect(standarditem.quality).toEqual(8)
+  describe("#define_multiplier", function() {
+    it("Sets multiplier to 1", function() {
+      const standarditem = new StandardItem('sword', 5, 10)
+      standarditem.define_multiplier();
+      expect(standarditem.multiplier).toEqual(1)
+    });
   });
 
-  it("Decreases the quality by 4 for conjured items", function() {
-    const standarditem = new StandardItem('Conjured sword', 0, 10)
-    standarditem.update();
-    expect(standarditem.quality).toEqual(6)
+  describe("#standard_decrease_quality", function() {
+    it("Decreases the quality by 1", function() {
+      const standarditem = new StandardItem('sword', 5, 10)
+      standarditem.standard_decrease_quality();
+      expect(standarditem.quality).toEqual(9)
+    });
   });
 
-  it("decreases the sellIn by 1", function() {
-    const standarditem = new StandardItem('sword', 5, 10)
-    standarditem.update();
-    expect(standarditem.sellIn).toEqual(4)
+  describe("#beyond_sellIn_decrease_quality", function() {
+    it("Decreases the quality by 2", function() {
+      const standarditem = new StandardItem('sword', 5, 10)
+      standarditem.beyond_sellIn_decrease_quality();
+      expect(standarditem.quality).toEqual(8)
+    });
+  });
+
+  describe("#calculate_update", function() {
+    it("Decreases quality by 1", function() {
+      const standarditem = new StandardItem('sword', 5, 10)
+      standarditem.calculate_update();
+      expect(standarditem.quality).toEqual(9)
+    });
+
+    it("Decreases quality by 2", function() {
+      const standarditem = new StandardItem('sword', 0, 10)
+      standarditem.calculate_update();
+      expect(standarditem.quality).toEqual(8)
+    });
+
+    it("Decreases quality by 1", function() {
+      const standarditem = new StandardItem('sword', 0, 1)
+      standarditem.calculate_update();
+      expect(standarditem.quality).toEqual(0)
+    });
+
+    it("Keeps quality at 0", function() {
+      const standarditem = new StandardItem('sword', 5, 0)
+      standarditem.calculate_update();
+      expect(standarditem.quality).toEqual(0)
+    });
   });
 });
